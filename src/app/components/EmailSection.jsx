7f4d5b499296d5fba66c69b51ae00fda7affcfbe
VikyArthya <vikyarthya1945@gmail.com>
2025-05-03 17:25:17 +0700

    fix navlink

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import GithubIcon from "../../../public/github-icon.png";
-import LinkedinIcon from "../../../public/linkedin-icon.png";
+import GithubIcon from "../../../public/github-icon.svg";
+import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 
